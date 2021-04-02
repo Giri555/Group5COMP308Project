43fc79cfe13732fc1e5c5a652be2d7e6ed9ab6da
Giri Siva <giri_siva51@outlook.com>
2021-04-02 16:20:12 -0400

    centered clips

--- React-Client/src/PatientPortal/MotivationalVideo.js
+++ React-Client/src/PatientPortal/MotivationalVideo.js
@@ -1,31 +1,27 @@
-import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Route, Redirect, withRouter } from 'react-router-dom';
+import React from 'react';
 import ReactPlayer from 'react-player';
-
-// import axios
-import axios from "axios";
 
 // import any react-bootstrap components you may need:
 import Container from 'react-bootstrap/Container';
 
-// import any of our components you may need:
-
-
-
-function MotivationalVideo(props){
-
-        return (
-                <Container>
-                    <h1 className='display-4 mt-3 text-center'>Motivational Videos</h1>
-                    <p className='text-muted text-center'>
-                            Get yourself healthy with these recommended videos.
-                    </p>
-                    <div>
-                            <ReactPlayer url="https://www.youtube.com/watch?v=T1XLHsz4r_M"/>
-                            <ReactPlayer url="https://www.youtube.com/watch?v=XtyLfJa3clI"/>                              
-                    </div>
+function MotivationalVideo() {
+    return (
+        <Container>
+            <h1 className='display-4 mt-3 text-center'>Motivational Videos</h1>
+            <p className='text-muted text-center'>
+                Get yourself healthy with these recommended videos.
+            </p>
+            <Container >
+                <Container className='my-3' style={{display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
+                    <ReactPlayer url='https://www.youtube.com/watch?v=T1XLHsz4r_M' />
                 </Container>
-        );
-};
+
+                <Container className='my-3' style={{display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
+                    <ReactPlayer url='https://www.youtube.com/watch?v=XtyLfJa3clI' />
+                </Container>
+            </Container>
+        </Container>
+    );
+}
 
 export default MotivationalVideo;
