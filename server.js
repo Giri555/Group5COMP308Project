7147612ad3aa7668f2a965f87e062fabdf6a8acb
Giri Siva <giri_siva51@outlook.com>
2022-10-06 14:24:13 -0400

    scaled using cluster module

--- server.js
+++ server.js
@@ -1,15 +1,35 @@
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 const port = 5000;
 // Load the module dependencies
-var mongoose = require('./config/mongoose'),
-    express = require('./config/express');
+const mongoose = require('./config/mongoose');
+const express = require('./config/express');
+const cluster = require('cluster');
+const os = require('os');
+
+const numberOfCPUs = os.cpus().length; // number of CPUs / cores
+
 // Create a new Mongoose connection instance
 var db = mongoose();
 // Create a new Express application instance
 var app = express();
-// Use the Express application instance to listen to the '3000' port
-app.listen(port);
-// Use the module.exports property to expose our Express application instance for external usage
-module.exports = app; //returns the application object
-// Log the server status to the console
-console.log(`Server running at http://localhost:${port}/`);
+
+if(cluster.isMaster)
+{
+        for(let i = 0; i < numberOfCPUs; i++)
+        {
+                cluster.fork();
+        }
+
+        // if a worker dies, start a new one
+        cluster.on('exit', (worker, code, signal) => {
+                console.log(`worker ${worker.process.pid} has died`);
+                cluster.fork();
+        });
+}
+else
+{
+        // Use the Express application instance to listen to the port
+        app.listen(port, ()=> console.log(`Server with PID: ${process.pid} running at http://localhost:${port}/`));
+}
+
+module.exports = app;
